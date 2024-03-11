59eaab1ddca2384bdc8c99dc08a201449a5b1d87
powerrewop <igor.rogi777@yandex.ru>
2024-03-11 18:22:02 +0800

    first commit

--- src/main/java/nay/kirill/generics/linkedList/LinkedList.java
+++ src/main/java/nay/kirill/generics/linkedList/LinkedList.java
@@ -1,7 +1,50 @@
 package nay.kirill.generics.linkedList;
 
-public class LinkedList {
+public class LinkedList<T> {
+    LinkedListТNode start = null;
+    LinkedListТNode end = null;
 
-    // TODO реализовать двусвязный список
+    public void add(T val){
 
+        if(end == null) {
+            LinkedListТNode newObj = new LinkedListТNode(val, null, null);
+            end = newObj;
+            start = newObj;
+        }else{
+            LinkedListТNode newObj = new LinkedListТNode(val, end, null);
+            end.next = newObj;
+            end = newObj;
+        }
+    }
+
+    public void printAll(){
+
+        LinkedListТNode myOb = start;
+
+        while (myOb != null) {
+
+            System.out.println(myOb.val);
+            myOb = getNext(myOb);
+        }
+    }
+
+    private LinkedListТNode getNext(LinkedListТNode obj){
+        return obj.next;
+    }
+
+    public void addAll(LinkedList<? extends T> t){
+        this.add((T) t.start.val);
+    }
+}
+
+class LinkedListТNode<T> {
+    T val;
+    LinkedListТNode prev;
+    LinkedListТNode next;
+
+    public LinkedListТNode(T val, LinkedListТNode prev, LinkedListТNode next){
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+    }
 }
